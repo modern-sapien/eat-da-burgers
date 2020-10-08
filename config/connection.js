6d936c47c2e676f1bcd9b4cbf15bf461a3390b4f
Modern Sapien <joncanales2@gmail.com>
2020-10-08 17:35:47 -0400

    Issues with connection have been resolved, should be running jawsDB.

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 const mysql = require("mysql");
 
 let connection;
+
   if(process.env.JAWSDB_URL)  {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
   }else {
-    mysql.createConnection({
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "1087",
     database: "burgers_db"
   });
-  
+
+
 connection.connect(function(err) {
 if (err) {
     console.error("error connecting: " + err.stack);
@@ -19,19 +21,8 @@ if (err) {
 }
 console.log("connected as id " + connection.threadId);
 });
-
+}
 
 module.exports = connection;
 
 
-
-// if(process.env.JAWSDB_URL)  {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL)
-// }else{
-//         connection = mysql.createConnection({
-//             host: "localhost",
-//             user: "root",
-//             PORT: "3306",
-            
-//         })
-// }
