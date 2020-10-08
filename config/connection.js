9d4e5b756b3b6abcbb24a56ae39c42548648d6dc
Modern Sapien <joncanales2@gmail.com>
2020-10-08 17:26:09 -0400

    bringing in JAWSDB.

---
+++ config/connection.js
@@ -0,0 +1,37 @@
+const mysql = require("mysql");
+
+let connection;
+  if(process.env.JAWSDB_URL)  {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  }else {
+    mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1087",
+    database: "burgers_db"
+  });
+  
+connection.connect(function(err) {
+if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+}
+console.log("connected as id " + connection.threadId);
+});
+
+
+module.exports = connection;
+
+
+
+// if(process.env.JAWSDB_URL)  {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL)
+// }else{
+//         connection = mysql.createConnection({
+//             host: "localhost",
+//             user: "root",
+//             PORT: "3306",
+            
+//         })
+// }
---
+++ config/orm.js
@@ -0,0 +1,41 @@
+
+const connection = require("./connection.js");
+
+function objToSQl(ob)   {
+    var arr = [];
+
+    for (va)
+}
+
+const orm = {
+ selectAll: function(cb)    {
+    const queryString = "SELECT * FROM burgers;"
+    connection.query(queryString, function(err, result) {
+        if (err) throw err;
+        cb(result);
+    });
+},
+
+insertOne: function(value, cb)    {
+    const queryString = "INSERT INTO burgers(burger_name) VALUES (?);"
+    connection.query(queryString, [value], function(err, result) {
+        if (err) throw err;
+        cb(result);
+    });
+},
+
+updateOne: function(value, cb)  {
+    const queryString = "UPDATE SET ? WHERE id = ?;"
+
+}
+
+// }
+    // * `selectAll()`
+    // * `insertOne()`
+    // * `updateOne()`
+
+
+}
+
+
+// INSERT INTO favorite_foods(food, score) VALUES ("lasagna", 3);
