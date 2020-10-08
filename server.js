7c5c2e803e6e3f8e10d6aaac16e6b60fd5149bd4
Modern Sapien <joncanales2@gmail.com>
2020-10-08 17:30:42 -0400

    updated connection with JAWSDB and exporting issues.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql")
+const connection = require(".config/connection");
 // const connection = require(".config/connection")
 const app = express();
 
