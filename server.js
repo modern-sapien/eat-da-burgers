540430dc36e1a7a9bb2b7158165ef95dfedce576
Modern Sapien <joncanales2@gmail.com>
2020-10-08 09:41:28 -0400

    Working on deploying handlebars

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// VIEWS ROUTES
+app.get("/", (req, res) =>  {
+    res.render("index")
+});
+
+
 app.get("/api/config",  (req, res)  =>  {
     res.json({
         success:true,
