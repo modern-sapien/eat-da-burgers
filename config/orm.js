4ff14d9721461b73a30f23d67194cb6512023cf2
Modern Sapien <joncanales2@gmail.com>
2020-10-09 19:30:06 -0400

    Cleaned up config file. Styling and pushing

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,6 @@
 const connection = require("./connection.js");
 
 
-// function objToSql(ob) {
-//     var arr = [];
-//     for (var key in ob) {
-//       var value = ob[key];
-//       if (Object.hasOwnProperty.call(ob, key)) {
-//         if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//           value = "'" + value + "'";
-//         }
-//         arr.push(key + "=" + value);
-//       }
-//     }
-// }
-
-// function printQuestionMarks(num) {
-//   var arr = [];
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
-//   return arr.toString();
-// }
 
 
 const orm = {
